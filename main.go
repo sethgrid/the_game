81695ba081178d4592df2b18d3ac7c0c35b78cfa
Seth Ammons <seth@sendgrid.com>
2014-12-23 13:07:39 -0800

    the game server proof of concept - multiple clients can connect and move about the maze

--- main.go
+++ main.go
@@ -4,113 +4,262 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
-
-	"github.com/sethgrid/curse"
+	"sync"
+	"time"
 )
 
-type coordinate struct {
-	x, y int
+type user struct {
+	userID        string
+	locationIndex int
+	position      position
 }
 
 type position struct {
-	backgroundColor int
-	color           int
+	x, y        int
+	closed      bool
+	description string
+	character   rune
+	userID      string
 }
 
-type screen struct {
-	width, height int
-	surface       map[coordinate]*position
-	cursor        *curse.Cursor
+func (p position) String() string {
+	return fmt.Sprintf("%d,%d", p.x, p.y)
+}
+
+type command struct {
+	cmd, userID string
+}
+
+type world struct {
+	locations []location
+	capacity  int
+	commands  []command
+	users     map[string]user
+
+	sync.Mutex
+}
+
+type location struct {
+	description string
+	display     []byte
+	positions   map[string]*position
+
+	sync.Mutex
 }
 
 func main() {
-	f, err := os.OpenFile("logfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+	log.Println("Starting")
+
+	listener := make(chan command)
+
+	loc := make([]location, 1)
+	loc[0] = location{
+		description: "init location",
+		display:     []byte("some map"),
+		positions:   loadMap("maps/map_1.map"),
 	}
-	defer f.Close()
-
-	log.SetOutput(f)
-	log.Println("starting")
-	_, err = curse.New()
-	if err != nil {
-		log.Fatal("unable to initialize curse environment - ", err)
+	commands := make([]command, 0)
+	w := &world{locations: loc,
+		capacity: 10,
+		commands: commands,
+		users:    make(map[string]user),
 	}
 
-	screenX, screenY, err := curse.GetScreenDimensions()
+	go gameRunner(w, listener)
+
+	http.HandleFunc("/", getWorld(w))
+	http.HandleFunc("/cmd", receiveCommand(listener))
+
+	log.Println("Registered /")
+	log.Println("Registered /cmd?uid=[string]&key=[char]")
+
+	log.Println("Listening on :8888")
+
+	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
-		log.Fatal("unable to get screen dimensions - ", err)
-	}
-
-	s := NewScreen(screenX, screenY)
-
-	s.cursor.Move(1, 1)
-	input := bufio.NewReader(os.Stdin)
-	for {
-		command, err := input.ReadByte()
-		if err != nil {
-			fmt.Println(err)
-		}
-		if string(command) == "w" {
-			s.cursor.MoveUp(1)
-			log.Println("w")
-		} else if string(command) == "s" {
-			s.cursor.MoveDown(1)
-			log.Println("w")
-		} else if string(command) == "a" {
-			s.cursor.MoveLeft(1)
-			log.Println("a")
-		} else if string(command) == "d" {
-			s.cursor.MoveRight(1)
-			log.Println("d")
-		} else if string(command) == "q" {
-			log.Println("q", "quiting")
-			s.cursor.Move(1, 1)
-			s.cursor.SetDefaultStyle()
-			s.cursor.EraseAll()
-			break
-		}
-		log.Println("new pos: ", s.cursor.Position)
-
-		s.Paint()
+		log.Fatal(err)
 	}
 }
 
-func NewScreen(x, y int) *screen {
-	newScreen := &screen{width: x, height: y}
-	newScreen.surface = make(map[coordinate]*position)
-	newScreen.cursor, _ = curse.New()
-	newScreen.Init()
-	return newScreen
-}
-
-func (s *screen) Init() {
-	for y := 1; y < s.height; y++ {
-		for x := 1; x < s.width; x++ {
-			s.surface[coordinate{x: x, y: y}] = &position{color: curse.RED}
-		}
+func getWorld(wrld *world) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		wrld.createUser(r.FormValue("uid"))
+		w.Write(wrld.display())
 	}
-	s.cursor.Move(1, 1)
-	log.Println("init pos:", s.cursor.Position)
-	s.Paint()
 }
 
-func (s *screen) Paint() {
-	currentX, currentY := s.cursor.Position.X, s.cursor.Position.Y
-	s.cursor.Move(1, 1)
-	s.cursor.EraseDown()
-	s.cursor.SetBackgroundColor(curse.WHITE).SetColor(curse.BLACK)
-	for y := 0; y < s.height; y++ {
-		s.cursor.EraseCurrentLine()
-		for x := 0; x < s.width; x++ {
-			if s.cursor.Position.X == x && s.cursor.Position.Y == y {
-				fmt.Printf("O")
-			} else {
-				fmt.Printf(".")
+func receiveCommand(listener chan command) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cmd := r.FormValue("key")
+		userID := r.FormValue("uid")
+		// validate
+		if userID == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("provide a uid string"))
+			return
+		}
+		if cmd == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("provide a key pressed"))
+			return
+		}
+
+		listener <- command{cmd: cmd, userID: userID}
+	}
+}
+
+func gameRunner(wrld *world, listener chan command) {
+	go func() {
+		for {
+			select {
+			case cmd := <-listener:
+				wrld.Lock()
+				wrld.commands = append(wrld.commands, cmd)
+				wrld.Unlock()
 			}
 		}
-		s.cursor.MoveDown(1)
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-time.Tick(time.Millisecond * 250):
+				wrld.updateBoard()
+			}
+		}
+	}()
+}
+
+func (wrld *world) createUser(userID string) {
+	startingPosition := position{x: 2, y: 3}
+
+	if _, found := wrld.users[userID]; !found {
+		log.Println("New user", userID)
+		wrld.users[userID] = user{userID: userID, position: startingPosition}
 	}
-	s.cursor.Move(currentX, currentY)
+}
+
+func (wrld *world) updateBoard() {
+	// play through commands and clear the list
+	wrld.Lock()
+	defer wrld.Unlock()
+
+	if len(wrld.commands) == 0 {
+		return
+	}
+
+	log.Println("Commands:")
+	for _, cmd := range wrld.commands {
+		log.Println(cmd)
+
+		wrld.createUser(cmd.userID)
+		curPos := wrld.users[cmd.userID].position
+		newPos := applyMove(curPos, cmd.cmd)
+
+		if wrld.locations[0].positions[newPos.String()].closed {
+			log.Printf("attempted location is closed (%s) -> (%s)\n", curPos.String(), newPos.String())
+			continue
+		}
+
+		// https://github.com/golang/go/issues/3117
+		// cannot yet assign to a field of a map indirectly
+		tmp_a := wrld.users[cmd.userID]
+		tmp_a.position = newPos
+		wrld.users[cmd.userID] = tmp_a
+
+		// update former/current position first. new pos may overwrite it,
+		// and we want to keep the most current information
+		tmp_c := wrld.locations[0].positions[curPos.String()]
+		tmp_c.closed = false
+		tmp_c.userID = ""
+		wrld.locations[0].positions[curPos.String()] = tmp_c
+
+		tmp_b := wrld.locations[0].positions[newPos.String()]
+		tmp_b.closed = true
+		tmp_b.userID = cmd.userID
+		wrld.locations[0].positions[newPos.String()] = tmp_b
+	}
+
+	// clear the played through commands
+	wrld.commands = make([]command, 0)
+	log.Println()
+}
+
+func (wrld *world) display() []byte {
+	body := make([]rune, 0)
+	width := 80
+	height := 20
+	for y := 1; y <= height; y++ {
+		for x := 1; x <= width; x++ {
+			cell := fmt.Sprintf("%d,%d", x, y)
+			pos := wrld.locations[0].positions[cell]
+			if pos == nil {
+				continue
+			}
+			if pos.userID != "" {
+				// todo: depending on user class, use different symbols and colors
+				body = append(body, '◊')
+			} else {
+				body = append(body, pos.character)
+			}
+
+		}
+		body = append(body, '\n')
+	}
+
+	return []byte(string(body))
+}
+
+func applyMove(p position, s string) position {
+	pNew := p
+	switch s {
+	case "mw":
+		pNew.y--
+	case "ma":
+		pNew.x--
+	case "ms":
+		pNew.y++
+	case "md":
+		pNew.x++
+	}
+	log.Printf("old pos, %+v, new pos %+v", p, pNew)
+	return pNew
+}
+
+func loadMap(path string) map[string]*position {
+	var err error
+	var Rune rune
+	theMap := make(map[string]*position)
+
+	fh, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	br := bufio.NewReader(fh)
+
+	x, y := 0, 1
+	for err == nil {
+		Rune, _, err = br.ReadRune()
+		x++
+		if Rune == rune('\n') {
+			y++
+			x = 0
+			continue
+		}
+		closed := false
+		if Rune != rune(' ') {
+			closed = true
+		}
+		theMap[fmt.Sprintf("%d,%d", x, y)] = &position{
+			x:         x,
+			y:         y,
+			character: Rune,
+			closed:    closed,
+		}
+
+	}
+
+	return theMap
 }
